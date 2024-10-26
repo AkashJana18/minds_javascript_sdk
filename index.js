6066522b0b9ba72e44bc865c48ea1c72825c70c9
AkashJana18 <akashjana663@gmail.com>
2024-10-26 19:04:40 +0530

    entry point

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+
+export { default as Client } from './minds/Client';
+export { default as DataService } from './minds/DataService';
+export { default as ExceptionHandling } from './minds/ExceptionHandling';
+export { default as Minds } from './minds/Minds';
